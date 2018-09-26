41d8f0e7c3cb3b5d7cdc68e11cef578d95b5a409
kwichmann <kristian.wichmann@gmail.com>
2018-09-26 20:43:49 +0200

    Vue client set up and simple home view done

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">Auth for noobs</a>
+    </nav>
+
+    <router-view class="container pt-3" />
+  
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home text-center">
+    <div class="jumbotron">
+      <h1 class="display-3">Auth for noobs</h1>
+      <p class="lead">
+        Learning auth is da bomb!
+      </p>
+      <hr class="my-4">
+      <p>
+        Coding along with CJ and Tony from Coding Garden.
+      </p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
